1534fcd635951355e34e304a2297e28154bbb151
Chinmayee259 <chinmayeeburte25@gmail.com>
2026-05-12 06:58:33 +0530

    Refactor BFS algorithm and input handling
    Refactored BFS implementation to simplify input handling and improve readability. Consolidated BFS logic into a single function and updated input prompts.

--- AI 2/AI2.py
+++ AI 2/AI2.py
@@ -1,61 +1,27 @@
-# AI 2. Implement Breadth First Search algorithm. Use an undirected graph and develop a recursive algorithm for searching all the vertices of a graph or tree data structure.
-def bfs_recursive(graph, queue, visited = None):
-    if not queue:  #If the queue is empty, stop the function
-        return
-    node = queue.pop(0)
-    print(node, end = " ")
-
-    for neighbor in graph.get(node, []):
-        if neighbor not in visited:
-            visited.add(neighbor)
-            queue.append(neighbor)
-    
-    bfs_recursive(graph, queue, visited)
-
-def bfs(graph, start):
-    visited = set([start])  # creates a set containing only the starting node.Mark the start node as already visited
-    queue = [start]
-    bfs_recursive(graph, queue, visited)
-
-
-
 graph = {}
 
-levels = int(input("\nEnter the number of levels : "))
+n = int(input("enter the number of the nodes : "))
 
-for i in range(levels):
-    n = int(input("\nEnter the number of nodes at level "+ str(i) + " : "))
-
-    for j in range(n):
-        node = input("Enter the node : ")
-        children = input("Enter the children of node " + node + " (space separated): ").split()
-
-        graph[node] = children
-
-start = input ("\nEnter the starting node : ")
-print("BFS : ", end = " ")
-bfs(graph, start)
+for  i in range(n):
+    node = input("enter the node : ")
+    neighbours = input("enter the neighbour of the node(split by space : ").split()
+    graph[node] = neighbours
     
+visited = set()
 
+def bfs(queue):
+    if queue:
+        node = queue.pop(0)
+        if node not in visited:
+            print(node,end = " ")
+            visited.add(node)
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                queue.append(neighbour)
+        bfs(queue)
 
-# --------------- OUTPUT ----------------------
-# Enter the number of levels : 3
+start = input("enter the starting node : ")
+queue = [start]
 
-# Enter the number of nodes at level 0 : 1
-# Enter the node : A
-# Enter the children of node A (space separated): B C
-
-# Enter the number of nodes at level 1 : 2
-# Enter the node : B
-# Enter the children of node B (space separated): D
-# Enter the node : C
-# Enter the children of node C (space separated): E
-
-# Enter the number of nodes at level 2 : 2
-# Enter the node : D
-# Enter the children of node D (space separated): 
-# Enter the node : E
-# Enter the children of node E (space separated): 
-
-# Enter the starting node : A
-# BFS :  A B C D E 
+print("bfs traversal : ")
+bfs(queue)
